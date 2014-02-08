2469993c441ce91be2a3e3bd297ab14ad1cb4c78
Arun Kutty <kutty.a@gmail.com>
2014-02-08 15:42:17 +0530

    Create Kollavarsham.java

---
+++ src/org/kollavarsham/Kollavarsham.java
@@ -0,0 +1,17 @@
+package org.kollavarsham;
+
+import java.util.Date;
+
+public class Kollavarsham extends Date {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Kollavarsham malayalamYear = new Kollavarsham();
+		System.out.println("The date is " + malayalamYear.getTime());
+
+	}
+
+}
